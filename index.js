8766a91d563992a677128cf32d6e220fb8b47d3f
Yussif Ramadan <r.yussif@wustl.edu>
2024-10-17 23:06:10 -0500

    Working street view

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-function initialize() {
-    const fenway = { lat: 42.345573, lng: -71.098326 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: fenway,
-      zoom: 14,
-    });
-    const panorama = new google.maps.StreetViewPanorama(
-      document.getElementById("pano"),
-      {
-        position: fenway,
-        pov: {
-          heading: 34,
-          pitch: 10,
-        },
-      },
-    );
-  
-    map.setStreetView(panorama);
-  }
-  
-  window.initialize = initialize;
+import { API_KEY } from "./config.js";
+
+
+function loadMap() {
+    const latitude = document.getElementById('latitude').value || '38.6463999'; 
+    const longitude = document.getElementById('longitude').value || '-90.3109969'; 
+
+  //  const mapUrl = `
+  //  https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=15&size=600x400&key=${API_KEY}
+ //   `;
+    const mapUrl=`https://maps.googleapis.com/maps/api/streetview?location=${latitude},${longitude}&size=600x400&key=${API_KEY}`;
+   
+    document.getElementById('map').src = mapUrl;
+}
+document.querySelector('.js-load-map').addEventListener('click', () => {
+  loadMap();
+})
