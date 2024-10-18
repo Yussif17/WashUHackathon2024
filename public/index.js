04f3d727d9bcf759479461c1c84409ea1fc7466b
ahmad8743 <a.h.hamzeh@wustl.edu>
2024-10-18 18:58:26 -0500

    firestore db

---
+++ public/index.js
@@ -0,0 +1,27 @@
+import { API_KEY } from "./config.js";
+
+document.addEventListener("DOMContentLoaded", event => {
+
+  const app = firebase.app();
+  const db = firebase.firestore();
+  const locations = db.collection('locations').doc('0');
+  console.log(locations);
+
+});
+
+function loadMap() {
+  const latitude = document.getElementById('latitude').value || '38.6463999'; 
+  const longitude = document.getElementById('longitude').value || '-90.3109969'; 
+
+  //const mapUrl = `
+  //https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=15&size=600x400&key=${API_KEY};
+
+  const mapUrl = `https://maps.googleapis.com/maps/api/streetview?location=${latitude},${longitude}&size=600x400&key=${API_KEY}`;
+   
+  document.getElementById('map').src = mapUrl;
+};
+  
+document.querySelector('.js-load-map').addEventListener('click', () => {
+    loadMap();
+});
+
